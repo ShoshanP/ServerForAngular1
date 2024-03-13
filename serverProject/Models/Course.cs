18bb510ff967ac03a7604d85a2405634495867fb
rachelP <racheli9467@gmail.com>
2024-03-14 00:14:32 +0200

    first commit

---
+++ serverProject/Models/Course.cs
@@ -0,0 +1,34 @@
+﻿namespace serverProject.Models
+{
+    public enum Study
+    {
+        Online,
+        Offline,
+        Hybrid
+    }
+    public class Course
+    {
+        public int id { get; set; }
+        public int? lecturer { get; set; }
+        public string? name { get; set; }
+        public Category? category { get; set; }
+        public int? countOfLessons { get; set; }
+        public DateTime? start { get; set; }
+        public List<string>? syllabus { get; set; }
+        public Study? study { get; set; }
+        public string? image { get; set; }
+
+        public Course(int id, int? lecturer, string? name, Category? category, int? countOfLessons, DateTime? start, List<string>? syllabus, Study? study, string? image)
+        {
+            this.id = id;
+            this.lecturer = lecturer;
+            this.name = name;
+            this.category = category;
+            this.countOfLessons = countOfLessons;
+            this.start = start;
+            this.syllabus = syllabus;
+            this.study = study;
+            this.image = image;
+        }
+    }
+}
